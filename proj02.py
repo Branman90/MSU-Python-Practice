18ebf37f5720eaf2f045f6860fd4fec8fecdc17d
Brandon  Potthoff <brandon.t.potthoff@gmail.com>
2014-11-04 19:40:58 -0700

    inital commit

---
+++ proj02.py
@@ -0,0 +1,15 @@
+print("Guess a solution for one of the following puzzles")
+print("Puzzle #1 SLAYER + SLAYER + SLAYER = LAYER")
+print("Puzzle #2 PEEP = P**E")
+
+usr_inpt = input("Enter the number of a puzzle:  ")
+
+if int(usr_inpt) == 1:
+	slayer_int = int(input("Enter your guess for SLAYER:   "))
+	#Code here for finding whether SLAYER value equals LAYERS
+	
+elif int(usr_input) == 2:
+	peep_int = int(input("Enter your guess for PEEP:  "))
+	#Code here for finding whether PEEP equals P**EOFError
+	
+print("Thanks for playing")
